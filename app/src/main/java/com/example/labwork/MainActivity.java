4de0ae658d083b8bf02c4b7c508849736c7c8e03
furymaxim <furymaxim@gmail.com>
2019-05-10 11:06:55 +0300

    final commit v1

--- app/src/main/java/com/example/labwork/MainActivity.java
+++ app/src/main/java/com/example/labwork/MainActivity.java
@@ -2,12 +2,42 @@ package com.example.labwork;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText mR;
+    Button mButton;
+    LinearLayout mLinearLayout;
+    TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Double RValue;
+
+        mR = findViewById(R.id.R);
+        mButton = findViewById(R.id.Solve);
+        mLinearLayout = findViewById(R.id.layout);
+        mTextView = findViewById(R.id.answer);
+
+        RValue = Double.valueOf(mR.getText().toString());
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Solution I = new Solution(RValue);
+                mLinearLayout.setVisibility(View.VISIBLE);
+                mTextView.setText(Double.toString(I.GetSolution()));
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/labwork/Solution.java
@@ -0,0 +1,29 @@
+package com.example.labwork;
+
+public class Solution {
+
+    private Double R;
+
+    Solution(double R) {
+        this.R = R;
+    }
+
+    public double GetSolution() {
+        double x, y, z, result;
+        int k = 1;
+        int N = 1000000;
+        for (int i = 0; i < N; i++) {
+            x = Math.random() * (R / Math.sqrt(2));
+            y = Math.random() * (R / Math.sqrt(2));
+            z = Math.random() * R;
+            if ((y >= 0) && (y <= Math.sqrt((R * R / 2) - x * x)) && (z >= Math.sqrt(x * x + y * y)) && (z <= Math.sqrt(R * R - x * x - y * y))) {
+                k = k + 1;
+            }
+        }
+        result = (double) k / N;
+        result = result * (R / Math.sqrt(2)) * (R / Math.sqrt(2)) * R;
+        return result;
+
+
+    }
+}
